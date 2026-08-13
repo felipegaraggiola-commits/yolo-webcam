1da7442ab34fc68534eeccaeb0499e8d6ff8a25e
felipegaraggiola-commits <felipe.garaggiola@init-lab.com.ar>
2026-08-13 20:24:35 -0300

    implemente la posibilidad de procesar frames de imagenes

--- detector.py
+++ detector.py
@@ -8,3 +8,27 @@ class YOLODetector:
         self.fuente_video = fuente_video
         self.umbral_confianza = umbral_confianza
         self.camara = None
+
+        def procesar_frame(self, frame):
+    resultados = self.modelo(
+        frame,
+        conf=self.umbral_confianza,
+        verbose=False
+    )
+
+    detecciones = []
+
+    for resultado in resultados:
+        for caja in resultado.boxes:
+            clase_id = int(caja.cls[0])
+            confianza = float(caja.conf[0])
+            nombre = self.modelo.names[clase_id]
+
+            detecciones.append({
+                "objeto": nombre,
+                "confianza": confianza
+            })
+
+    frame_anotado = resultados[0].plot()
+
+    return frame_anotado, detecciones
