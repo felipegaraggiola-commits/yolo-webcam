43741aa1c2079d3f1f88056802c263da0a6e7cad
felipegaraggiola-commits <felipe.garaggiola@init-lab.com.ar>
2026-08-13 20:21:54 -0300

    Cree tres parametros configurables, logrando configurar el detector

--- detector.py
+++ detector.py
@@ -3,4 +3,8 @@ from ultralytics import YOLO
 
 
 class YOLODetector:
-    pass
+    def __init__(self, modelo, fuente_video=0, umbral_confianza=0.5):
+        self.modelo = YOLO(modelo)
+        self.fuente_video = fuente_video
+        self.umbral_confianza = umbral_confianza
+        self.camara = None
